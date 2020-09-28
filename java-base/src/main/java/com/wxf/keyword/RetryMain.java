07b44a1f2a37bc0d2afe4ce26d664b752f692a7b
WangMaoSong <390871264@qq.com>
2020-09-28 09:41:08 +0800

    Retry用法

---
+++ java-base/src/main/java/com/wxf/keyword/RetryMain.java
@@ -0,0 +1,48 @@
+package com.wxf.keyword;
+
+/**
+ * Retry测试
+ *
+ * continue用法
+ * 不使用Retry时输出：
+ * 0  1  2  4  5  6  7
+ * 0  1  2  4  5  6  7
+ * 0  1  2  4  5  6  7
+ * 0  1  2  4  5  6  7
+ * 0  1  2  4  5  6  7
+ *
+ * 使用Retry时输出：
+ * 0  1  2  0  1  2  0  1  2  0  1  2  0  1  2
+ *
+ *
+ * Break用法
+ *
+ * 不使用Retry时输出：
+ * 0  1  2
+ * 0  1  2
+ * 0  1  2
+ * 0  1  2
+ * 0  1  2
+ *
+ * 使用Retry时输出：
+ * 0  1  2
+ *
+ * @author WangMaoSong
+ * @since  2020年9月28日 09:25:33
+ */
+public class RetryMain {
+
+    public static void main(String[] args) {
+        retry:
+        for (int i = 0; i < 5; i++) {
+            for (int j = 0; j < 8; j++) {
+                if (j == 3)
+                    break retry;
+                System.out.print(j);
+                System.out.print("  ");
+            }
+            System.out.println();
+        }
+
+    }
+}
