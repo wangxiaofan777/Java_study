f32117ffd1099fafb240559e62c1dd805aacdbf1
wangxiaofan777 <390871264@qq.com>
2021-04-30 09:14:58 +0800

    初始化缓存服务HA&商品服务

---
+++ eshop-product/src/test/java/com/wxf/product/eshop/product/EshopProductApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wxf.product.eshop.product;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EshopProductApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
