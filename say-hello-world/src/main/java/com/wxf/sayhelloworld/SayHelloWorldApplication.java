736b97b881510350b40a15682d3d4473ea7b5448
wangxiaofan777 <390871264@qq.com>
2021-05-13 13:27:36 +0800

    将服务注册到Eureka

---
+++ say-hello-world/src/main/java/com/wxf/sayhelloworld/SayHelloWorldApplication.java
@@ -0,0 +1,26 @@
+package com.wxf.sayhelloworld;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@EnableEurekaClient
+@SpringBootApplication
+public class SayHelloWorldApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SayHelloWorldApplication.class, args);
+    }
+
+    @Value("${server.port}")
+    private String port;
+
+    @GetMapping("/hello")
+    public String hello(String name) {
+        return "hello," + name + " from port:" + port;
+    }
+}
