58fd7ba13e95c2db25085edf35b180cf8f9aa12b
wangxiaofan777 <390871264@qq.com>
2021-05-14 15:42:10 +0800

    Hystrix熔断

--- greeting-service/src/main/java/com/wxf/greetingservice/GreetingServiceApplication.java
+++ greeting-service/src/main/java/com/wxf/greetingservice/GreetingServiceApplication.java
@@ -2,14 +2,20 @@ package com.wxf.greetingservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
 @EnableEurekaClient
 @EnableFeignClients
+@EnableHystrix
+@EnableCircuitBreaker
+@EnableHystrixDashboard
 @SpringBootApplication
 public class GreetingServiceApplication {
 
